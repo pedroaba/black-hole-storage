21afbaf818e33845a5b20a52cdf20d29f66ead27
Pedro Augusto B. Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-09-08 18:34:51 -0300

    refact: remove trpc on nextjs project

--- apps/web/src/lib/trpc/react.ts
+++
@@ -1,6 +0,0 @@
-import type { AppRouter } from '@bhs/trpc'
-import { createTRPCReact } from '@trpc/react-query'
-
-export const trpc = createTRPCReact<AppRouter>()
-
-export const TRPCProvider = trpc.Provider
